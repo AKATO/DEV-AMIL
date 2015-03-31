aa5f8e474c1ae3435f3c22886d6ad5c85f22f0a5
Arthur Kato <arthur_kato@hotmail.com>
2015-03-31 13:49:29 -0300

    Entendendo o proxiamento com CGLIB e explorando o fucionamento do Hibernate em cima metodos proxiados na persistencia de Objetos do modelo

--- src/main/java/br/com/akato/hibernate/proxy/MyProxyCGLIBService.java
+++ src/main/java/br/com/akato/hibernate/proxy/MyProxyCGLIBService.java
@@ -1,22 +1,24 @@
 package br.com.akato.hibernate.proxy;
 
+import net.sf.cglib.proxy.Enhancer;
 import br.com.akato.hibernate.Pessoa;
 
 public class MyProxyCGLIBService<T> {
-	public MyFakeList criarListaFake(){
-		final MyInterceptor c = new MyInterceptor();
+	public MyFakeList<T> criarListaFake(){
+		final MyFakeList<T> exemplo = new MyFakeList<T>();
+		MyInterceptor c = new MyInterceptor(exemplo);
 		MyFakeList objetoProxiado = (MyFakeList) Enhancer.create(MyFakeList.class,c);
 		return objetoProxiado;
 	}
 	
 	public static void main(String[] args) {
 		MyFakeList<Pessoa> list = new MyProxyCGLIBService<Pessoa>().criarListaFake();
-		list.add(new Pessoa(4l));
-		list.add(new Pessoa(5l));
-		list.add(new Pessoa(6l));
+		list.add(new Pessoa(1l));
+		list.add(new Pessoa(2l));
+		list.add(new Pessoa(3l));
 		int qtdeItens = list.getSize();
-		Pessoa p = list.get(6);
-		System.out.println(String.format("Id: %d ", p.getNome()));
+		Pessoa p = list.get(2);
+		System.out.println(String.format("Id: %d ", p.getId()));
 		System.out.println(String.format("Qtde Itens na lista: %d ", qtdeItens));
 		}
 }
